6c0e81b3a407c1ca2d8bc64f3271b9ac021b8212
Thomas Nibler <dev@tnibler.de>
2024-09-24 23:32:38 +0200

    wip and also wrong

--- src/local_extrema.rs
+++ src/local_extrema.rs
@@ -141,10 +141,18 @@ unsafe fn local_extrema_avx2(
     let ny = arr.shape()[1];
     let nz = arr.shape()[0];
     assert!(nz == 3);
-    let mut scratch = vec![0u8; 2 * 1024];
-    // a <= 0: a <= b => a - b <= 0
-    // a >= 0: a >= b => a - b >= 0
-    // a-b, shr 31,  not mask with sign bit
+    // TODO: MUST BE ALIGNED to alignof(u64)
+    // bitflags marking extrema for 256 pixel positions
+    let mut extr_flag_acc = [0u8; 32];
+    // number of elements in `extr_flag_acc`
+    let mut extr_flag_cnt = 0;
+    // position in image corresponding to first bit in `extr_flag_acc`
+    let mut extr_flac_acc_start = border;
+
+    // if register of 8 f32's reaches outside of current image row, how many values are in image
+    // bounds
+    let mut use_values: usize;
+
     use std::arch::x86_64::*;
     let p0 = arr.as_slice().unwrap()[..(ny * nx)].as_ptr();
     let p1 = arr.as_slice().unwrap()[(ny * nx)..(2 * ny * nx)].as_ptr();
@@ -152,240 +160,143 @@ unsafe fn local_extrema_avx2(
     let ny = arr.shape()[1] as isize;
     let nx = arr.shape()[2] as isize;
     let mut extrema: Vec<RetainedExtremum> = Vec::default();
-    let mut buf_idx: usize = 0;
-    let mut buf_start: usize = (nx as usize * border) + border;
     let vvalue_threshold = _mm256_set1_ps(value_threshold);
     let sign_bit_mask = _mm256_castsi256_ps(_mm256_set1_epi32(i32::MAX));
-    unsafe {
-        for y in (border as isize)..(ny - border as isize) {
-            let mut x = border as isize;
+    for y in (border as isize)..(ny - border as isize) {
+        let mut x = border as isize;
+        use_values = 0;
+        let mut last_of_row = false;
 
-            while x < nx - border as isize {
-                let dist_from_border = nx - border as isize - x;
-                if dist_from_border < 8 {
-                    x -= 8 - dist_from_border;
-                    buf_idx -= (8 - dist_from_border) as usize;
-                }
-                if (buf_idx + nx as usize) >= scratch.len() {
-                    // scan through buffer to process extrema
-                    let tail = buf_idx % 32;
-                    for i in (0..(buf_idx - tail)).step_by(32) {
-                        let extr = _mm256_loadu_si256(scratch.as_ptr().add(i) as *mut __m256i);
-                        let bitmask = _mm256_movemask_epi8(extr);
-                        if bitmask == 0 {
-                            continue;
-                        }
-                        for j in 0..32 {
-                            if (bitmask >> j) & 1 == 1 {
-                                let ey = (buf_start + i + j) / (nx as usize);
-                                let ex = (buf_start + i + j) - (nx as usize * ey);
-                                assert!(ey < ny as usize);
-                                assert!(ex < nx as usize);
-                                if let Some(res) = discard_or_interpolate_extremum(
-                                    ScaleSpacePoint {
-                                        scale,
-                                        x: ex,
-                                        y: ey,
-                                    },
-                                    arr,
-                                    dog,
-                                ) {
-                                    extrema.push(res);
-                                }
-                            }
-                        }
+        extr_flac_acc_start = border;
+        while x < nx - border as isize {
+            let dist_from_border = nx - border as isize - x;
+            if dist_from_border < 8 {
+                last_of_row = true;
+                extr_flag_acc[extr_flag_cnt..].fill(0);
+                x -= 8 - dist_from_border;
+                use_values = dist_from_border as usize;
+            }
+            let val = _mm256_loadu_ps(p1.offset(y * nx + x));
+
+            // zero out sign bit to take absolute value
+            let abs = _mm256_and_ps(val, sign_bit_mask);
+            let gt_thresh = _mm256_cmp_ps::<_CMP_GT_OQ>(abs, vvalue_threshold);
+
+            macro_rules! mmax {
+                ($a:expr, $b:expr) => {
+                    _mm256_max_ps($a, $b)
+                };
+            }
+            macro_rules! mmin {
+                ($a:expr, $b:expr) => {
+                    _mm256_min_ps($a, $b)
+                };
+            }
+            let v0 = _mm256_loadu_ps(p1.offset(y * nx + x - 1)); // left
+            let v1 = _mm256_loadu_ps(p1.offset(y * nx + x + 1)); // right
+            let v2 = _mm256_loadu_ps(p1.offset((y - 1) * nx + x - 1)); // above left
+            let v3 = _mm256_loadu_ps(p1.offset((y - 1) * nx + x)); // above
+            let v4 = _mm256_loadu_ps(p1.offset((y - 1) * nx + x + 1)); // above right
+            let v5 = _mm256_loadu_ps(p1.offset((y + 1) * nx + x - 1)); // below left
+            let v6 = _mm256_loadu_ps(p1.offset((y + 1) * nx + x)); // below
+            let v7 = _mm256_loadu_ps(p1.offset((y + 1) * nx + x + 1)); // below right
+
+            // Tree shaped max computation to minimize dependencies between instructions
+            let vmax = mmax!(
+                mmax!(mmax!(v0, v1), mmax!(v2, v3)),
+                mmax!(mmax!(v4, v5), mmax!(v6, v7))
+            );
+            let vmin = mmin!(
+                mmin!(mmin!(v0, v1), mmin!(v2, v3)),
+                mmin!(mmin!(v4, v5), mmin!(v6, v7))
+            );
+            let le_all = _mm256_and_ps(gt_thresh, _mm256_cmp_ps::<_CMP_LE_OQ>(val, vmin));
+            let ge_all = _mm256_and_ps(gt_thresh, _mm256_cmp_ps::<_CMP_GE_OQ>(val, vmax));
+
+            let v0 = _mm256_loadu_ps(p0.offset(y * nx + x - 1)); // left
+            let v1 = _mm256_loadu_ps(p0.offset(y * nx + x));
+            let v2 = _mm256_loadu_ps(p0.offset(y * nx + x + 1)); // right
+            let v3 = _mm256_loadu_ps(p0.offset((y - 1) * nx + x - 1)); // above left
+            let v4 = _mm256_loadu_ps(p0.offset((y - 1) * nx + x)); // above
+            let v5 = _mm256_loadu_ps(p0.offset((y - 1) * nx + x + 1)); // above right
+            let v6 = _mm256_loadu_ps(p0.offset((y + 1) * nx + x - 1)); // below left
+            let v7 = _mm256_loadu_ps(p0.offset((y + 1) * nx + x)); // below
+            let v8 = _mm256_loadu_ps(p0.offset((y + 1) * nx + x + 1)); // below right
+
+            let vmax = mmax!(
+                mmax!(mmax!(v0, v1), mmax!(v2, v3)),
+                mmax!(mmax!(v4, v5), mmax!(v6, mmax!(v7, v8)))
+            );
+            let vmin = mmin!(
+                mmin!(mmin!(v0, v1), mmin!(v2, v3)),
+                mmin!(mmin!(v4, v5), mmin!(v6, mmin!(v7, v8)))
+            );
+            let le_all = _mm256_and_ps(le_all, _mm256_cmp_ps::<_CMP_LE_OQ>(val, vmin));
+            let ge_all = _mm256_and_ps(ge_all, _mm256_cmp_ps::<_CMP_GE_OQ>(val, vmax));
+
+            let v0 = _mm256_loadu_ps(p2.offset(y * nx + x - 1)); // left
+            let v1 = _mm256_loadu_ps(p2.offset(y * nx + x));
+            let v2 = _mm256_loadu_ps(p2.offset(y * nx + x + 1)); // right
+            let v3 = _mm256_loadu_ps(p2.offset((y - 1) * nx + x - 1)); // above left
+            let v4 = _mm256_loadu_ps(p2.offset((y - 1) * nx + x)); // above
+            let v5 = _mm256_loadu_ps(p2.offset((y - 1) * nx + x + 1)); // above right
+            let v6 = _mm256_loadu_ps(p2.offset((y + 1) * nx + x - 1)); // below left
+            let v7 = _mm256_loadu_ps(p2.offset((y + 1) * nx + x)); // below
+            let v8 = _mm256_loadu_ps(p2.offset((y + 1) * nx + x + 1)); // below right
+
+            let vmax = mmax!(
+                mmax!(mmax!(v0, v1), mmax!(v2, v3)),
+                mmax!(mmax!(v4, v5), mmax!(v6, mmax!(v7, v8)))
+            );
+            let vmin = mmin!(
+                mmin!(mmin!(v0, v1), mmin!(v2, v3)),
+                mmin!(mmin!(v4, v5), mmin!(v6, mmin!(v7, v8)))
+            );
+            let le_all = _mm256_and_ps(le_all, _mm256_cmp_ps::<_CMP_LE_OQ>(val, vmin));
+            let ge_all = _mm256_and_ps(ge_all, _mm256_cmp_ps::<_CMP_GE_OQ>(val, vmax));
+
+            let is_extr = _mm256_or_ps(le_all, ge_all);
+
+            extr_flag_acc[extr_flag_cnt] = _mm256_movemask_ps(is_extr).to_le_bytes()[0];
+            extr_flag_cnt += 1;
+
+            if last_of_row {
+                extr_flag_acc[use_values..].fill(0)
+            }
+            if extr_flag_cnt == extr_flag_acc.len() - 1 || last_of_row {
+                for i in (0..extr_flag_acc.len()).step_by(8) {
+                    let mut qw = *(extr_flag_acc.as_ptr().add(i) as *const u64);
+                    if qw == 0 {
+                        continue;
                     }
-                    scratch[buf_idx - tail..]
-                        .iter()
-                        .take(tail)
-                        .enumerate()
-                        .filter_map(|(idx, extr)| {
-                            if *extr != 0 {
-                                Some(idx + buf_idx - tail)
-                            } else {
-                                None
-                            }
-                        })
-                        .for_each(|idx| {
-                            let ey = (buf_start + idx) / (nx as usize);
-                            let ex = (buf_start + idx) - (nx as usize * ey);
-                            assert!(ey < ny as usize);
-                            assert!(ex < nx as usize);
-                            if let Some(res) = discard_or_interpolate_extremum(
-                                ScaleSpacePoint {
-                                    scale,
-                                    x: ex,
-                                    y: ey,
-                                },
-                                arr,
-                                dog,
-                            ) {
-                                extrema.push(res);
-                            }
-                        });
-                    buf_start = (y * nx + x) as usize;
-                    buf_idx = 0;
-                    scratch.fill(0);
+                    let qw_start = (extr_flac_acc_start + i * 8) as u64;
+                    while qw != 0 {
+                        let trlz = _tzcnt_u64(qw);
+                        let extr_x = qw_start + trlz;
+                        if let Some(extr) = discard_or_interpolate_extremum(
+                            ScaleSpacePoint {
+                                scale,
+                                x: extr_x as usize,
+                                y: y as usize,
+                            },
+                            arr,
+                            dog,
+                        ) {
+                            extrema.push(extr);
+                        }
+                        qw ^= 1 << trlz;
+                    }
                 }
-                let val = _mm256_loadu_ps(p1.offset(y * nx + x));
-
-                // zero out sign bit to take absolute value
-                let abs = _mm256_and_ps(val, sign_bit_mask);
-                let gt_thresh = _mm256_cmp_ps::<_CMP_GT_OQ>(abs, vvalue_threshold);
-
-                macro_rules! mmax {
-                    ($a:expr, $b:expr) => {
-                        _mm256_max_ps($a, $b)
-                    };
-                }
-                macro_rules! mmin {
-                    ($a:expr, $b:expr) => {
-                        _mm256_min_ps($a, $b)
-                    };
-                }
-                let v0 = _mm256_loadu_ps(p1.offset(y * nx + x - 1)); // left
-                let v1 = _mm256_loadu_ps(p1.offset(y * nx + x + 1)); // right
-                let v2 = _mm256_loadu_ps(p1.offset((y - 1) * nx + x - 1)); // above left
-                let v3 = _mm256_loadu_ps(p1.offset((y - 1) * nx + x)); // above
-                let v4 = _mm256_loadu_ps(p1.offset((y - 1) * nx + x + 1)); // above right
-                let v5 = _mm256_loadu_ps(p1.offset((y + 1) * nx + x - 1)); // below left
-                let v6 = _mm256_loadu_ps(p1.offset((y + 1) * nx + x)); // below
-                let v7 = _mm256_loadu_ps(p1.offset((y + 1) * nx + x + 1)); // below right
-
-                // Tree shaped max computation to minimize dependencies between instructions
-                let vmax = mmax!(
-                    mmax!(mmax!(v0, v1), mmax!(v2, v3)),
-                    mmax!(mmax!(v4, v5), mmax!(v6, v7))
-                );
-                let vmin = mmin!(
-                    mmin!(mmin!(v0, v1), mmin!(v2, v3)),
-                    mmin!(mmin!(v4, v5), mmin!(v6, v7))
-                );
-                let le_all = _mm256_and_ps(gt_thresh, _mm256_cmp_ps::<_CMP_LE_OQ>(val, vmin));
-                let ge_all = _mm256_and_ps(gt_thresh, _mm256_cmp_ps::<_CMP_GE_OQ>(val, vmax));
-
-                let v0 = _mm256_loadu_ps(p0.offset(y * nx + x - 1)); // left
-                let v1 = _mm256_loadu_ps(p0.offset(y * nx + x));
-                let v2 = _mm256_loadu_ps(p0.offset(y * nx + x + 1)); // right
-                let v3 = _mm256_loadu_ps(p0.offset((y - 1) * nx + x - 1)); // above left
-                let v4 = _mm256_loadu_ps(p0.offset((y - 1) * nx + x)); // above
-                let v5 = _mm256_loadu_ps(p0.offset((y - 1) * nx + x + 1)); // above right
-                let v6 = _mm256_loadu_ps(p0.offset((y + 1) * nx + x - 1)); // below left
-                let v7 = _mm256_loadu_ps(p0.offset((y + 1) * nx + x)); // below
-                let v8 = _mm256_loadu_ps(p0.offset((y + 1) * nx + x + 1)); // below right
-
-                let vmax = mmax!(
-                    mmax!(mmax!(v0, v1), mmax!(v2, v3)),
-                    mmax!(mmax!(v4, v5), mmax!(v6, mmax!(v7, v8)))
-                );
-                let vmin = mmin!(
-                    mmin!(mmin!(v0, v1), mmin!(v2, v3)),
-                    mmin!(mmin!(v4, v5), mmin!(v6, mmin!(v7, v8)))
-                );
-                let le_all = _mm256_and_ps(le_all, _mm256_cmp_ps::<_CMP_LE_OQ>(val, vmin));
-                let ge_all = _mm256_and_ps(ge_all, _mm256_cmp_ps::<_CMP_GE_OQ>(val, vmax));
-
-                let v0 = _mm256_loadu_ps(p2.offset(y * nx + x - 1)); // left
-                let v1 = _mm256_loadu_ps(p2.offset(y * nx + x));
-                let v2 = _mm256_loadu_ps(p2.offset(y * nx + x + 1)); // right
-                let v3 = _mm256_loadu_ps(p2.offset((y - 1) * nx + x - 1)); // above left
-                let v4 = _mm256_loadu_ps(p2.offset((y - 1) * nx + x)); // above
-                let v5 = _mm256_loadu_ps(p2.offset((y - 1) * nx + x + 1)); // above right
-                let v6 = _mm256_loadu_ps(p2.offset((y + 1) * nx + x - 1)); // below left
-                let v7 = _mm256_loadu_ps(p2.offset((y + 1) * nx + x)); // below
-                let v8 = _mm256_loadu_ps(p2.offset((y + 1) * nx + x + 1)); // below right
-
-                let vmax = mmax!(
-                    mmax!(mmax!(v0, v1), mmax!(v2, v3)),
-                    mmax!(mmax!(v4, v5), mmax!(v6, mmax!(v7, v8)))
-                );
-                let vmin = mmin!(
-                    mmin!(mmin!(v0, v1), mmin!(v2, v3)),
-                    mmin!(mmin!(v4, v5), mmin!(v6, mmin!(v7, v8)))
-                );
-                let le_all = _mm256_and_ps(le_all, _mm256_cmp_ps::<_CMP_LE_OQ>(val, vmin));
-                let ge_all = _mm256_and_ps(ge_all, _mm256_cmp_ps::<_CMP_GE_OQ>(val, vmax));
-
-                let is_extr = _mm256_or_ps(le_all, ge_all);
-
-                // pack lowest byte of every f32 in mask into 8 bytes
-                let shuf = _mm256_set_epi8(
-                    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 8, 4, 0, -1, -1, -1, -1,
-                    -1, -1, -1, -1, -1, -1, -1, -1, 12, 8, 4, 0,
-                );
-
-                let shuffled = _mm256_shuffle_epi8(_mm256_castps_si256(is_extr), shuf);
-                let packed = _mm256_permutevar8x32_epi32(
-                    shuffled,
-                    _mm256_set_epi32(-1, -1, -1, -1, -1, -1, 4, 0),
-                );
-
-                let out_loc = scratch.as_mut_ptr().add(buf_idx);
-                let store_mask = _mm256_set_epi64x(0, 0, 0, i64::MIN);
-                _mm256_maskstore_epi64(out_loc as *mut i64, store_mask, packed);
-
-                x += 8;
-                buf_idx += 8;
+                extr_flac_acc_start = x as usize + 8;
+                extr_flag_cnt = 0;
             }
-            buf_idx += 2 * border;
+
+            x += 8;
         }
     }
-    buf_idx -= border;
-    assert_eq!(buf_start + buf_idx, (nx as usize) * (ny as usize - border));
-    let tail = buf_idx % 32;
-    for i in (0..(buf_idx - tail)).step_by(32) {
-        let extr = _mm256_loadu_si256(scratch.as_ptr().add(i) as *mut __m256i);
-        let bitmask = _mm256_movemask_epi8(extr);
-        if bitmask == 0 {
-            continue;
-        }
-        for j in 0..32 {
-            if (bitmask >> j) & 1 == 1 {
-                let ey = (buf_start + i + j) / (nx as usize);
-                let ex = (buf_start + i + j) - (nx as usize * ey);
-                assert!(ey < ny as usize);
-                assert!(ex < nx as usize);
-                if let Some(res) = discard_or_interpolate_extremum(
-                    ScaleSpacePoint {
-                        scale,
-                        x: ex,
-                        y: ey,
-                    },
-                    arr,
-                    dog,
-                ) {
-                    extrema.push(res);
-                }
-            }
-        }
-    }
-    scratch[buf_idx - tail..]
-        .iter()
-        .take(tail)
-        .enumerate()
-        .filter_map(|(idx, extr)| {
-            if *extr != 0 {
-                Some(idx + buf_idx - tail)
-            } else {
-                None
-            }
-        })
-        .for_each(|idx| {
-            let ey = (buf_start + idx) / (nx as usize);
-            let ex = (buf_start + idx) - (nx as usize * ey);
-            assert!(ey < ny as usize);
-            assert!(ex < nx as usize);
-            if let Some(res) = discard_or_interpolate_extremum(
-                ScaleSpacePoint {
-                    scale,
-                    x: ex,
-                    y: ey,
-                },
-                arr,
-                dog,
-            ) {
-                extrema.push(res);
-            }
-        });
+    assert_eq!(
+        extr_flac_acc_start + extr_flag_cnt,
+        nx as usize - border as usize
+    );
     extrema
 }
